d4bdae248a7ebbbca454dd8f3c8a33034fa9bccb
Colin Power <colinjpower1@gmail.com>
2022-12-01 10:04:32 -0600

    Initial Commit

---
+++ UncommonApp/UncommonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UncommonAppApp.swift
+//  UncommonApp
+//
+//  Created by Colin Power on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct UncommonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
